9a54351ea5bb2ae1aa0bf7816473c0c385e74ed4
jose garcia orozco <josegoupm@gmail.com>
2011-10-30 21:41:23 +0100

    create empty functions

--- precio_justo/frwk/getScore.py
+++ precio_justo/frwk/getScore.py
@@ -9,5 +9,40 @@ import time
 class getScore(webapp.RequestHandler):
     
     def get(self):
+        user= users.get_current_user()
+        returned=Constants.ERROR_NO_USER
+        listArguments=self.request.arguments()
+        if len(listArguments)>0:
+            argumentData=listArguments.pop()
+            if argumentData==Constants.ARG_MYSCORE :
+                returned=self.getUserDataScore()
+            elif argumentData==Constants.ARG_ARCHIEVEMENTS:
+                returned=self.getArchievements()
+            elif argumentData==Constants.ARG_TOTALRANKING:
+                returned=self.getTotalRanking()
+            elif argumentData==Constants.ARG_MONTHRANKIN:
+                returned=self.getMonthRanking()
+            elif argumentData==Constants.ARG_GLOBALTIME:
+                returned=self.getGlobalTime()
+            else :
+                returned=Constants.ERROR_BAD_ARGUMENT
+            
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(Constants.ERROR_BAD_REQUEST)
+        self.response.out.write(returned)
+        
+    def getGlobalTime(self):
+        return '{ "timelstamp": "'+str(time.time())+'"}'
+  
+        
+    def getUserDataScore(self):
+        return ''
+    
+    def getArchievements(self):
+        return ''
+    
+    def getTotalRanking(self):
+        return ''
+    
+    def getMonthRanking(self):
+        return ''
+        
